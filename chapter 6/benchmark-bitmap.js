2bca524c34e54f6990c037c6547e558cb2ffc57e
Hugo Lopes Tavares <hltbra@gmail.com>
2015-09-08 00:22:14 -0400

    Add code samples of chapter 6

---
+++ chapter 6/benchmark-bitmap.js
@@ -0,0 +1,15 @@
+var redis = require("redis");
+var client = redis.createClient();
+var MAX_USERS = 100000;
+var MAX_DEALS = 12;
+var MAX_DEAL_ID = 10000;
+
+for (var i = 0 ; i < MAX_USERS ; i++) {
+  var multi = client.multi();
+  for (var j = 0 ; j < MAX_DEALS ; j++) {
+    multi.setbit("bitmap:user:" + i, MAX_DEAL_ID - j, 1);
+  }
+  multi.exec();
+}
+
+client.quit();
